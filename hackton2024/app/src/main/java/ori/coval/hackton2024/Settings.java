a925a916f7dfd54af69258eb50b05387b8aeae76
Noam Goldfarb <nonofarb@gmail.com>
2024-02-05 05:08:12 +0200

    Added see contacts

--- hackton2024/app/src/main/java/ori/coval/hackton2024/Settings.java
+++ hackton2024/app/src/main/java/ori/coval/hackton2024/Settings.java
@@ -2,14 +2,22 @@ package ori.coval.hackton2024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Settings extends AppCompatActivity {
 
     private Button change;
+    ImageButton btnImage;
+
+    private static final int PICK_CONTACT_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,36 @@ public class Settings extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnImage = findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickContact();
+            }
+        });
+
     }
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_CONTACT_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data != null && data.getData() != null) {
+                // Handle the selected contact URI
+                Uri selectedContactUri = data.getData();
+
+                // Do something with the selected contact URI (e.g., save it, process it)
+                // For now, let's just log it
+                Log.d("SelectedContact", "Contact URI: " + selectedContactUri.toString());
+            }
+        }
+    }
+
 }
