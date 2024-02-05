23e50d6cff85f2a268f59dab82ebaa1c4b7b9431
Noam Goldfarb <nonofarb@gmail.com>
2024-02-05 02:14:10 +0200

    added logo

--- hackton2024/app/src/main/java/ori/coval/hackton2024/alert.java
+++ hackton2024/app/src/main/java/ori/coval/hackton2024/alert.java
@@ -2,13 +2,30 @@ package ori.coval.hackton2024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class alert extends AppCompatActivity {
+
+    Intent intent;
+    Button btnBreath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
+
+        btnBreath = findViewById(R.id.btnBreath);
+        btnBreath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(alert.this, stressReduce.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
